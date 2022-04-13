88777645d03b39c838aa5cfd0975a87dd7f47438
Joao E Pereira Jr <joaoepj@gmail.com>
2022-04-13 17:46:30 +0000

    Add CLI subcommands

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ mod env_var;
 mod assert_test;
 mod codec_demo;
 
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+
 use serde::Deserialize;
 use std::fs;
 use clap::{Parser, Subcommand};
@@ -20,15 +24,28 @@ use clap::{Parser, Subcommand};
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[clap(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
+struct Cli {
+    //
+    #[clap(subcommand)]
+    command: Commands,
 }
+
+#[derive(Debug,Subcommand)]
+enum Commands {
+    /// Read config from toml file
+    CfgFromToml,
+    /// Read config from environment variables
+    EnvVar,
+    /// Test Parity Scale CODEC
+    CodecDemo,
+    /// Test assert
+    Assert,
+    /// Mini redis client built with tokio async library
+    TokioMrCli,
+    /// Mini redis server built with tokio async library
+    TokioMrSrv
+}
+
 
 
 
@@ -47,16 +64,57 @@ struct Books {
 
 
 fn main() {
-    let args = Args::parse();
-    //match args.name {
-    //    "codec-"
-    //}
+    let cli = Cli::parse();
 
-    codec_demo::run();
-    let content = fs::read_to_string("../../src/library.example")
-        .expect("Something went wrong reading the file");
-    let library: Library = toml::from_str(content.as_str()).unwrap();
-    println!("{:#?}", library);
-    println!("{:#?}", env_var::unwrap("ENV_VAR"));
-    println!("Assert: {:#?}",assert_test::run());
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::CfgFromToml => {
+            let content = fs::read_to_string("../../src/library.example")
+                .expect("Something went wrong reading the file");
+            let library: Library = toml::from_str(content.as_str()).unwrap();
+            println!("{:#?}", library);
+        }
+        Commands::EnvVar => {
+           println!("{:#?}", env_var::unwrap("ENV_VAR"));
+        }
+        Commands::CodecDemo => {
+           codec_demo::run();
+        }
+        Commands::Assert => {
+           println!("Assert: {:#?}",assert_test::run());
+        }
+        Commands::TokioMrCli => {
+           let mut rt = tokio::runtime::Runtime::new().unwrap();
+                    rt.block_on(async {
+                    println!("Aqui é tokio rapah!!!");
+                })
+        }
+        Commands::TokioMrSrv => {
+           let mut rt = tokio::runtime::Runtime::new().unwrap();
+                    rt.block_on(async {
+                        let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+                        loop {
+                            // The second item contains the IP and port of the new connection.
+                            let (socket, _) = listener.accept().await.unwrap();
+                            process(socket).await;
+                        }
+                    })
+        }
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // The `Connection` lets us read/write redis **frames** instead of
+    // byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        // Respond with an error
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
